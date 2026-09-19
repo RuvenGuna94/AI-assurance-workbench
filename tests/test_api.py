9f46ed651eeef7198b055e6cdafbc480ab511426
Ruven Guna <ruvenguna94@hotmail.com>
2026-09-19 16:02:30 +0800

    test: updated testing file to address bug

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,10 @@
+from fastapi.testclient import TestClient
+
+from support_gateway import api
+
+
+client = TestClient(api.app)
+
 AUTH_HEADERS = {
     "Authorization": f"Bearer {api.APP_API_KEY}",
 }
